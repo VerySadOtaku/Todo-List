a2c64710ef11fc221df8656d1ea2c2b79e00a497
VerySadOtaku <german.sichenikov@gmail.com>
2021-08-08 22:23:43 +0300

    :rocket: First commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="hello">
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  *{
+    box-sizing: border-box;
+  }
+
+  .container{
+    max-width: 600px;
+    margin: 0px auto;
+  }
+</style>
